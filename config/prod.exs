5de6a28ad45de01627835d9581617f969aaa8f90
Praveen Perera <ppraveen25@gmail.com>
2020-03-11 13:32:37 -0400

    Start server on release

--- config/prod.exs
+++ config/prod.exs
@@ -1,5 +1,7 @@
 import Config
 
-config :covid, CovidWeb.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
+config :covid, CovidWeb.Endpoint,
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
 
 config :logger, level: :info
